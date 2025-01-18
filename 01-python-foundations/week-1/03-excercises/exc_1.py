3d0c1e2b879c2d5cb418832318273e5f78dbf292
liz <lizalfond@gmail.com>
2025-01-18 16:05:47 -0500

    add first excercise file

---
+++ 01-python-foundations/week-1/03-excercises/exc_1.py
@@ -0,0 +1,9 @@
+def test_environment
+	"""""""""
+	Simple function to verify python env is working
+	"""""""""
+
+	Print("Python environment is successfully configured")
+
+if __name__ == "__main__":
+	test_environment()
